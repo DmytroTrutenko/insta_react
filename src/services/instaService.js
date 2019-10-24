36c90b417433b5780a8175060a0339b9b7bb2130
DmytroTrutenko <55551711+DmytroTrutenko@users.noreply.github.com>
2019-10-24 20:46:05 +0300

    add `` in instaService

--- src/services/instaService.js
+++ src/services/instaService.js
@@ -7,7 +7,7 @@ export default class instaService{
         const res = await fetch(`${this._apiBase}${url}`);  //выучить fetch api, async, await
         
         if(!res.ok){
-            throw new Error('Could not fetch ${url}; received ${res.status}');  //создаем ошибку
+            throw new Error(`Could not fetch ${url}; received ${res.status}`); 
         }
 
         return await res.json();   //превращаем ответ сервера в json формат
